f900d40ea0aa7c55f1d7fa66ef961c6a3b666fea
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-06-03 15:10:15 -0500

    add error handling test for useOrders hook with MSW

--- src/hooks/useOrdersMsw.test.tsx
+++ src/hooks/useOrdersMsw.test.tsx
@@ -1,8 +1,10 @@
-import { Await, MemoryRouter } from 'react-router-dom'
+import { HttpResponse, http } from 'msw'
 import { Mock, beforeEach, describe, expect, it, vi } from 'vitest'
 import { SessionProvider, useSession } from '../context/AuthContext'
 import { renderHook, waitFor } from '@testing-library/react'
 
+import { MemoryRouter } from 'react-router-dom'
+import { server } from '../mocks/server'
 import useOrders from './useOrders'
 
 vi.mock('../context/AuthContext', async () => {
@@ -37,6 +39,24 @@ describe('useOrders MSW', () => {
         await waitFor(() => {
             const lengthOrders = result.current.orders.length;
             expect(lengthOrders).toBe(1)
+        })
+    })
+
+    it('Debe obtener error', async () => {
+        server.use(
+            http.get("http://localhost:3001/orders", () => {
+                return new HttpResponse(null, {
+                    status: 500,
+                    statusText: 'Internal Server Error'
+                })
+            })
+        )
+
+        const { result } = renderHook(() => useOrders(), { wrapper })
+
+        await waitFor(() => {
+            const error = result.current.error;
+            expect(error).toBe("Failed to fetch orders. Please try again later.")
         })
     })
 })
